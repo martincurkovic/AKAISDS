2eee2806aaafa4363960fb200acb0e91d13910f6
Martin Curkovic <martincurkovic@icloud.com>
2026-08-04 19:33:53 +1000

    updated linux and macos build scripts to assume yes for downloads

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -32,7 +32,7 @@ if [ ! -f "pysidedeploy.spec" ]; then
   sed -i '' 's/^title = .*/title = AKAISDS/' pysidedeploy.spec
   sed -i '' 's|^icon = .*|icon = ../assets/icon/AKAISDS-macos.icns|' pysidedeploy.spec
   sed -i '' 's/^mode = .*/mode = standalone/' pysidedeploy.spec
-  sed -i '' 's|^extra_args = .*|extra_args = --quiet --noinclude-qt-translations --macos-app-name=AKAISDS --include-module=mido.backends.rtmidi --include-data-dir=ui/icons=ui/icons --include-data-files=ui/style.qss.template=ui/style.qss.template|' pysidedeploy.spec
+  sed -i '' 's|^extra_args = .*|extra_args = --quiet --noinclude-qt-translations --assume-yes-for-downloads --macos-app-name=AKAISDS --include-module=mido.backends.rtmidi --include-data-dir=ui/icons=ui/icons --include-data-files=ui/style.qss.template=ui/style.qss.template|' pysidedeploy.spec
 
   echo "Spec generated and configured."
 fi
